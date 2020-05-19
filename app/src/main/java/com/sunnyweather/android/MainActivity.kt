e2506af291aaf3a583d672d77bfbec2a6b21a175
shijunhong <meair@qianyansoft.com>
2020-05-19 16:03:28 +0800

    配置application

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -2,7 +2,7 @@ package com.sunnyweather.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//AvpxjNkxTD8kEvI1
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
